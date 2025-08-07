2df7fe8f9942e34c6fc0b23be54f979f56dde62f
Rahma Messadi <rahma.messadimess23@gmail.com>
2025-08-07 12:24:32 +0100

    delete prometheus config

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 import os
 from flask import Flask, request, jsonify
 import json
-from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
-# Initialize metrics ONCE at module level
-metrics = PrometheusMetrics(app, group_by='endpoint')
-metrics.register_default()  # This prevents duplicate registration
 
 # Environment config
 DATA_DIR = os.getenv('DATA_DIR', '/data')
 TASKS_FILE = os.path.join(DATA_DIR, os.getenv('TASKS_FILENAME', 'tasks.json'))
 SECRET_MESSAGE = os.getenv('SECRET_MESSAGE', 'No secret')
 PORT = int(os.getenv('PORT', 5000))
-
-
 
 @app.route('/')
 def index():
